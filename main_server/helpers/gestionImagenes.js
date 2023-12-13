e043e48ac88cfcbcbb5ef5ca18ff754506e09eb8
Ahrisu <arisuodelagtv@gmail.com>
2023-12-12 20:04:56 -0600

    Implementación del CRUD de productos completa. Adición de una carpeta de imágenes de prueba.

---
+++ main_server/helpers/gestionImagenes.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+
+// Inserción de imágenes
+async function uploadImg(path, product){
+    try {
+        const result = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload(path, async (error, result) => {
+                if(error){
+                    reject(error);
+                }
+                else{
+                    resolve(result);
+                    // Agregar el public_id al string de imágenes del producto
+                    product.imagenProducto = result.public_id;
+                }
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Eliminar imágenes
+async function removeImage(publicId) {
+    try {
+      const result = await cloudinary.uploader.destroy(publicId);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {
+    uploadImg,
+    removeImage
+}
